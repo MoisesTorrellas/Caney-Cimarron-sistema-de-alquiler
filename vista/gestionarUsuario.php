d431b40686c8dd8e82a0783499b1492aba5c431d
MoisesTorrellas <moitcj@gmail.com>
2024-05-25 13:34:02 -0400

    Moises 25/5/2024 1:33pm
    Avancé del modal

--- vista/gestionarUsuario.php
+++ vista/gestionarUsuario.php
@@ -10,9 +10,34 @@
         <div class="conteinerHijo">
             <?php include('comunes/navbarsuperior.php');?>
             <div class="content">
-                <h1>Gestionar Usuario</h1>
+                <div class="tituloBox">
+                    <h1 class="titulo">Gestionar Usuarios<span>.</span></h1>
+                </div>
+                <div class="contentBusqueda">
+                    <div class="buscador">
+                        <input class="" type="search" id="buscar" name="buscar" placeholder="Buscar...">
+                        <i class="fi fi-br-search lupa"></i>
+                    </div>
+                    <div class="botonBox">
+                        <button  type="button" id="incluir" data-toggle="modal" data-target="#modal1" name="consultar" class="boton"><i class='fi fi-br-user-add iconB'></i> Nuevo Usuario</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
+    <div class="modal fade" tabindex="-1" role="dialog"  id="modal1">
+        <div class="modalInter modal-dialogo" role="document">
+            <div class="contentModal">
+                <div class="headerModal">
+                    <h5 class="modal-title">Formulario de Personas</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
